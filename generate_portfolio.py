255831bce6ec892e47f5dc112f1f2bc2f0d61440
Gurbo86 <leonel.maguet@gmail.com>
2025-05-04 22:27:28 +0100

    Setup page for myself and added work experience.

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
 
-# Load JSON data
+# Load JSON data - this should be the path to your JSON file by default portfolio.json
 with Path("portfolio.json").open(encoding="utf-8") as f:
     data = json.load(f)
 
@@ -19,8 +19,10 @@ if "social_links" in data:
 
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader("."), autoescape=True)
-index_template = env.get_template("index_template.html")
-resume_template = env.get_template("resume_template.html")
+# Load the CV template - this should be the path to your HTML file by default CV_template.html
+index_template = env.get_template("FullCV.html")
+# Load the resume template - this should be the path to your HTML file by default resume_template.html
+resume_template = env.get_template("resumeCV.html")
 
 # Render the template with the data
 html_output = index_template.render(**data)
@@ -31,9 +33,11 @@ resume_output = resume_template.render(**data)
 # resume_output = resume_template.render(name=data["name"], label=data["label"]...)
 
 # Write the output to an HTML file
+# write the file established for your cv - this is the path to your HTML file by default index.html
 with Path("index.html").open("w", encoding="utf-8") as f:
     f.write(html_output)
 
+# write the file established for your resume - this is the path to your HTML file by default resume.html
 with Path("resume.html").open("w", encoding="utf-8") as f:
     f.write(resume_output)
 
